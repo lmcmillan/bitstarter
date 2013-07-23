4a90ed7fc67ff8574514a245adf26ca33f68bae4
Lachy McMillan <lachy.mcm@gmail.com>
2013-07-23 11:40:37 +1000

    web.js bugfix readfile encoding incorrect

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
-var index = fs.readFileSync('index.html', 'String')
+var index = fs.readFileSync('index.html', 'utf8')
 
 app.get('/', function(request, response) {
 	response.send(index);
