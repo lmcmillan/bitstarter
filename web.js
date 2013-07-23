c90671949edccd4ae7d320a4f6ecf03eba69c60b
Lachy McMillan <lachy.mcm@gmail.com>
2013-07-23 11:16:22 +1000

    web.js now reads from index.html

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
+var index = fs.readFileSync('index.html', 'String')
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	response.send(index);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
